a23b10f4f4e5d88b4f907f8da38f003200053949
Al Drum <Alexterpns@gmail.com>
2023-03-07 20:35:31 +0200

    updated to be compatible with new hand_class

--- training/mediapipe_static_images.py
+++ training/mediapipe_static_images.py
@@ -10,14 +10,14 @@ from natsort import natsorted,ns,os_sorted
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
-final_data = np.empty(shape=[0,21,3])
-_image_set_path = ".\\image_set\\"
+final_data = np.empty(shape=[0,42])
+_image_set_path = ".\\image_set\\a\\"
 
 IMAGE_FILES = []
 
 for content in os_sorted(os.listdir(_image_set_path)):     #get image names
   if content.endswith(".jpg"):
-    IMAGE_FILES.append(".\\image_set\\"+content)
+    IMAGE_FILES.append(".\\image_set\\a\\"+content)
 
 #natsorted(IMAGE_FILES, alg=ns.IGNORECASE)
 
@@ -52,8 +52,9 @@ with mp_hands.Hands(
       #       f'{hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].y * image_height}, '
       #       f'{hand_landmarks.landmark[mp_hands.HandLandmark.WRIST].z })'
       #   )
-        current_ =  Hand(idx,hand_landmarks)
-        current_data = current_.landmark_data
+        current_ =  Hand(hand_landmarks)
+        current_data = np.asarray(current_.landmark_data)
+        print("SHAPE:",current_data.shape)
       
       image_height, image_width, _ = image.shape
       annotated_image = image.copy()
@@ -67,15 +68,15 @@ with mp_hands.Hands(
       cv2.imwrite(
         '.\\image_set\\annotated_image_set\\annotated_image' + str(idx) + '.png', cv2.flip(annotated_image, 1))
       
-    final_data = np.append(final_data,[current_data], axis =0)
+    final_data = np.append(final_data,[current_data[0]], axis =0)
       
     print(final_data)      
 
 
-  with open(".\\training\\result.csv", mode='w+') as csv_file: #saving line per frame in csv
+  with open(".\\training\\a.csv", mode='w+') as csv_file: #saving line per frame in csv
     
     for index, per_image_result in enumerate(final_data):
-      per_image_result=per_image_result.reshape(1,63)
+      per_image_result=per_image_result.reshape(1,42)
       np.savetxt(csv_file, per_image_result,delimiter=',')
     
 
